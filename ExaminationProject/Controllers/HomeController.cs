f87f668c4b9147ede21e2f8c761e8f87ef69ff07
İbrahim Fətəliyev <ibrahim.fataliyev.c@asoiu.edu.az>
2023-05-11 11:50:35 +0400

    Quesiton's_design_&_admin_panel's_table_design_fixxed

--- ExaminationProject/Controllers/HomeController.cs
+++ ExaminationProject/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using System.Diagnostics;
 
 namespace ExaminationProject.Controllers
 {
-    //[Authorize(Policy = "IsNotDeletedPolicy")]
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -32,29 +32,27 @@ namespace ExaminationProject.Controllers
         }
 
         [HttpGet("examcategory/{id}")]
+        [Authorize(Policy = "IsNotDeletedPolicy")]
         public IActionResult ExamCategory(int id)
         {
-            var questions = _context.Questions.Where(x => x.ExamCategoryId == id).ToList();
+            var questions = _context.Questions.Where(x => x.ExamCategoryId == id).Where(x => x.IsDeleted == false).ToList();
             var questionIds = questions.Select(x => x.Id).ToList();
             var questionAnswers = _context.QuestionAnswers
                 .Include(x => x.Answer)
                 .Where(x => questionIds.Contains(x.QuestionId))
                 .ToList();
             var answers = questionAnswers.Select(x => x.Answer).Distinct().ToList();
-
+            var categoryName = _context.ExamCategories.Where(x => x.Id == id).First().CategoryName;
             var vm = new ExamCategoryVM
             {
                 SelectedCategoryId = id,
                 Questions = questions,
                 Answers = answers,
-                QuestionAnswers = questionAnswers
+                QuestionAnswers = questionAnswers,
+                CategoryName = categoryName,
             };
 
             return View(vm);
-        }
-        public IActionResult Privacy()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- ExaminationProject/ViewModels/ExamCategoryVM.cs
+++ ExaminationProject/ViewModels/ExamCategoryVM.cs
@@ -5,6 +5,7 @@ namespace ExaminationProject.ViewModels
     public class ExamCategoryVM
     {
         public int SelectedCategoryId { get; set; }
+        public string CategoryName { get; set; }
         public List<Question> Questions { get; set; }
         public List<Answer> Answers { get; set; }
         public List<QuestionAnswer> QuestionAnswers { get; set; }
